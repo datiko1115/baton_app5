e785faeb09705c52fa149c8d34418c02e0d67fe4
datiko1115 <datiko1115@gmail.com>
2022-02-24 22:28:27 +0900

    保存できるようになった

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,18 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end 
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end 
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :image, :description, :category_id, :price, :condition_id, :recipient_id, :prefecture_id, :shipment_id,).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,18 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  validates :item_name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :price, presence: true
+  validates :condition_id, presence: true  
+  validates :recipient_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipment_id, presence: true
+
+  # validates :price, numericality: { in: 18..65 }
 end
