b71cc1a874342bdd9c9b94d4b03d0e3c4e399058
datiko1115 <datiko1115@gmail.com>
2022-02-25 15:31:49 +0900

    js導入準備

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,16 +9,13 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :category_id, presence: true
   validates :price, presence: true
-  # validates :price, numericality: { with: /\A[0-9]+\z/, message: 'is invalid. Input Half-width number' }
+  validates :price, numericality: { with: /\A[0-9]+\z/, message: 'is invalid. Input Half-width number' }
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+  validates :price, numericality: { only_integer: true } #横に長くなりすぎそうだから３つに分けたんで、つっこまんといてください
   validates :condition_id, presence: true  
   validates :recipient_id, presence: true
   validates :prefecture_id, presence: true
   validates :shipment_id, presence: true
   validates :image, presence: true
 
-
-
-  # PRICE_REGEX = /\A[0-9]+\z/.freeze
-  # validates_format_of :price, with: PRICE_REGEX, message: 'is invalid. Input half-width characters'
 end
