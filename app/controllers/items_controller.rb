6b8d3decc6c96820aa97928fc30d4a995b0f4a6b
datiko1115 <datiko1115@gmail.com>
2022-02-27 15:15:46 +0900

    コントローラーにデストロいアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,6 +34,9 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+  end  
+
   private
   def item_params
     params.require(:item).permit(:item_name, :image, :description, :category_id, :price, :condition_id, :recipient_id, :prefecture_id, :shipment_id,).merge(user_id: current_user.id)
