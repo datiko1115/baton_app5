8fd48d5514ec689a6d720f0c98010230abdac033
datiko1115 <datiko1115@gmail.com>
2022-02-27 14:59:48 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show, :edit]
+  before_action :set_item, only: [:edit, :show, :update]
+  before_action :move_to_index, except: [:index, :show]
   before_action :ensure_correct_user, only:[:edit]
 
   def index
@@ -20,24 +21,16 @@ class ItemsController < ApplicationController
   end 
 
   def show
-    @item = Item.find(params[:id])
   end 
 
   def edit
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
       render :edit
-    end
-  end
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to new_user_session_path
     end
   end
 
@@ -46,10 +39,19 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:item_name, :image, :description, :category_id, :price, :condition_id, :recipient_id, :prefecture_id, :shipment_id,).merge(user_id: current_user.id)
   end
 
-  def ensure_correct_user
+  def set_item
     @item = Item.find(params[:id])
-     unless @item.user == current_user
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
+  def ensure_correct_user
+    unless @item.user == current_user
      redirect_to items_path
-   end
+    end
   end
 end
