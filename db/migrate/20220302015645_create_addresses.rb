d2349f8953a0409eeccec41662b5347d7573af67
datiko1115 <datiko1115@gmail.com>
2022-03-02 11:17:34 +0900

    各モデルにテーブルを作成

--- db/migrate/20220302015645_create_addresses.rb
+++ db/migrate/20220302015645_create_addresses.rb
@@ -2,6 +2,12 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
 
+      t.string :postcode,               null: false
+      t.integer :prefecture_id,         null: false
+      t.address :string,                null: false
+      t.string :building,               null: false
+      t.string :phone,                  null: false
+      t.order :references,              null: false, foreign_key: true
       t.timestamps
     end
   end
