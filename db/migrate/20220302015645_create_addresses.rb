f708ceb0f6bfdee96831d2bc93aeeada85fda91d
datiko1115 <datiko1115@gmail.com>
2022-03-02 12:08:35 +0900

    商品詳細ページから購入画面に遷移できるようにした

--- db/migrate/20220302015645_create_addresses.rb
+++ db/migrate/20220302015645_create_addresses.rb
@@ -4,10 +4,10 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
 
       t.string :postcode,               null: false
       t.integer :prefecture_id,         null: false
-      t.address :string,                null: false
-      t.string :building,               null: false
+      t.string :address,                null: false
+      t.string :building
       t.string :phone,                  null: false
-      t.order :references,              null: false, foreign_key: true
+      t.references :order,              null: false, foreign_key: true
       t.timestamps
     end
   end
