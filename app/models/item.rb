a6cfea0e29f009fddcea4dcfde2f3b7c8ab07a55
datiko1115 <datiko1115@gmail.com>
2022-03-07 21:27:19 +0900

    複数毎機能実装の途中

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_one_attached :image
+  has_many_attached :images
   has_one :order
 
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -21,6 +21,6 @@ class Item < ApplicationRecord
   validates :recipient_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipment_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :image, presence: true
+  validates :images, presence: true
 
 end
