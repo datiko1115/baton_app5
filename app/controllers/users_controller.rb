ecaea01934c4d1819dd3aa3b76ea5c0ec4a15400
datiko1115 <datiko1115@gmail.com>
2022-03-14 22:32:07 +0900

    コントローラーに渡すデータを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+  def show
+    @first_name = current_user.first_name
+    @last_name = current_user.last_name
+    @items = current_user.items
+  end
 end
