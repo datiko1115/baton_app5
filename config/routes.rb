b763d60c681f14953fd16d94e56e8babaa0bcef7
datiko1115 <datiko1115@gmail.com>
2022-03-17 18:09:31 +0900

    管理者ユーザー機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, controllers: {
-    sessions:      'admin_users/sessions',
-    passwords:     'admin_users/passwords',
-    registrations: 'admin_users/registrations'
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
   }
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
+  
   root to: 'items#index'
   resources :users, only: [:show, :edit, :update] 
   resources :users, only: :show
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_17_015848) do
+ActiveRecord::Schema.define(version: 2022_03_17_083113) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -46,17 +46,17 @@ ActiveRecord::Schema.define(version: 2022_03_17_015848) do
     t.index ["order_id"], name: "index_addresses_on_order_id"
   end
 
-  create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "admin_name"
+    t.string "admin_name", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_admin_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
