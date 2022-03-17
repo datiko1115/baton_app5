dc9b384d81956c86d63e9488b83e8e5f472a473c
datiko1115 <datiko1115@gmail.com>
2022-03-17 21:07:43 +0900

    Update admins_controller.rb

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,5 @@
 class AdminsController < ApplicationController
-  before_action :authenticate_admin![:show]
+  before_action :authenticate_admin!, only:[:show]
   def show
     @admin = Admin.find(params[:id])
     @adminname = @admin.admin_name
