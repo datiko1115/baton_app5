67b99b17c4e041e2e66d081339d4521d4ac38f4b
datiko1115 <datiko1115@gmail.com>
2022-03-17 15:33:49 +0900

    管理者ユーザー登録機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, controllers: {
+    sessions:      'admin_users/sessions',
+    passwords:     'admin_users/passwords',
+    registrations: 'admin_users/registrations'
+  }
   devise_for :users
   root to: 'items#index'
   resources :users, only: [:show, :edit, :update] 
