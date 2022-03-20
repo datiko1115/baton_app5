32c703d2703df50e7dc3d90572d8e4f79f3e8488
datiko1115 <datiko1115@gmail.com>
2022-03-21 01:37:37 +0900

    買付品出品機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,13 +2,13 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many_attached :images
   has_one :order
+  has_one :admin_item
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :prefecture
   belongs_to :shipment
   belongs_to :recipient
-  belongs_to :condition
 
   validates :item_name, presence: true
   validates :description, presence: true
@@ -17,7 +17,6 @@ class Item < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
   validates :price, numericality: { only_integer: true } 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :recipient_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipment_id, numericality: { other_than: 1, message: "can't be blank" }
--- db/migrate/20220224031643_create_items.rb
+++ db/migrate/20220224031643_create_items.rb
@@ -5,7 +5,6 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.text :description,               null: false
       t.integer :category_id,            null: false
       t.integer :price,                  null: false
-      t.integer :condition_id,           null: false
       t.integer :recipient_id,           null: false
       t.integer :prefecture_id,          null: false
       t.integer :shipment_id,            null: false
