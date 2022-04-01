f3b10a3e422d1547054d78f9a6d423e0f630727a
datiko1115 <datiko1115@gmail.com>
2022-04-01 11:50:53 +0900

    c

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_20_071606) do
+ActiveRecord::Schema.define(version: 2022_04_01_024726) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
