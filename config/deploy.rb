c381d4ebae1af2a8cfdcf271687dcba742a87a49
datiko1115 <datiko1115@gmail.com>
2022-04-01 15:22:29 +0900

    a

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.17.0'
 
 # Capistranoのログの表示に利用する
 set :application, 'baton_app5'
-
+set :branch, 'aaaaaa'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:datiko1115/baton_app5.git'
 
