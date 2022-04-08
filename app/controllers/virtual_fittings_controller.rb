e9c1a522eecbc86c1c96dae8833f73b9f8f0517e
datiko1115 <datiko1115@gmail.com>
2022-04-08 15:14:37 +0900

    chmodのテスト

--- app/controllers/virtual_fittings_controller.rb
+++ app/controllers/virtual_fittings_controller.rb
@@ -44,6 +44,7 @@ class VirtualFittingsController < ApplicationController
 
       result.format("jpg")
       if Rails.env.production?
+        result.chmod（0644、"testfile"、"out"） 
         result.write("public/assets/virtualfitting#{current_user.id}.jpg")
         logger.debug "http://54.168.8.242/assets/virtualfitting#{current_user.id}.jpg"
       else
